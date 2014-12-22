1851fc668646c93b7b1aeac9c694c4d607d08a33
Anders Schau Knatten <anders@knatten.org>
2014-12-22 23:06:49 +0100

    Other file too

---
+++ main2.cpp
@@ -0,0 +1,190 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include <unistd.h>
+
+
+int X() { return 1; }
+int Y() { return 2; }
+
+TEST_CASE( "2Factorials are computed", "[factorial]" ) {
+    SECTION("first") {
+        //REQUIRE( X() == Y());
+        SECTION("second") {
+            REQUIRE( 10 == 10);
+        }
+    }
+}
+
+TEST_CASE("2Other test")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 2")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 3")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 4")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 5")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 6")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 7")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 8")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 9")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 10")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 11")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 12")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 13")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 14")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 15")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 16")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 17")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 18")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 19")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 20")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 21")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 22")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 23")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 24")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 25")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 26")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 27")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 28")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("2Other test 29")
+{
+    usleep(2 * 1000 * 1000);
+    REQUIRE(1 == 1);
+}
